e74ab69b18645c9bae8b03d81450a1c87108e177
yuting <yuting.wu@hyweb.com.tw>
2019-06-13 12:52:18 +0800

    add tkinter layour

--- ex_tkinter/example_03_tkinter_layout.py
+++ ex_tkinter/example_03_tkinter_layout.py
@@ -3,6 +3,23 @@ from tkinter import ttk
 
 
 """
-本範例:
-
+本範例: tkinter 
+文件連結:
 """
+
+
+class Cool_Gui:
+    def __init__(self, master):
+        self.master = master
+        self._create_gui()
+
+    def _create_gui(self):
+        self.master.configure(background='#3CB371')
+        # 視窗大小
+        self.master.geometry("500x400")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    Cool_Gui(root)
+    root.mainloop()
